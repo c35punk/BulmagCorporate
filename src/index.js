bc56be56ef4a43b85ceef3bb733a128c24b1d103
Liubomir Djantov <ldjantov@gmail.com>
2019-11-17 11:58:20 +0200

    major update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/bulmag-design-system-react.scss";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Index from "views/Index.jsx";
+import Landing from "views/examples/Landing.jsx";
+import Login from "views/examples/Login.jsx";
+import Profile from "views/examples/Profile.jsx";
+import Register from "views/examples/Register.jsx";
+import Contacts from "views/examples/Contacts.jsx";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={props => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        exact
+        render={props => <Landing {...props} />}
+      />
+      <Route path="/login-page" exact render={props => <Login {...props} />} />
+      <Route
+        path="/profile-page"
+        exact
+        render={props => <Profile {...props} />}
+      />
+      <Route
+        path="/register-page"
+        exact
+        render={props => <Register {...props} />}
+      />
+      <Route
+        path="/contacts"
+        exact
+        render={props => <Contacts {...props} />}
+      />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
