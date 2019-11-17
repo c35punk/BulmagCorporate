b1e2346e598ffcfef2ea1417fd131357a4b82a91
Liubomir Djantov <ldjantov@gmail.com>
2019-11-17 21:24:10 +0200

    updated components

---
+++ src/views/Index.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Row } from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.jsx";
+import CardsFooter from "components/Footers/CardsFooter.jsx";
+
+// index page sections
+import Hero from "./IndexSections/Hero.jsx";
+import Buttons from "./IndexSections/Buttons.jsx";
+import Inputs from "./IndexSections/Inputs.jsx";
+import CustomControls from "./IndexSections/CustomControls.jsx";
+import Menus from "./IndexSections/Menus.jsx";
+import Navbars from "./IndexSections/Navbars.jsx";
+import Tabs from "./IndexSections/Tabs.jsx";
+import Progress from "./IndexSections/Progress.jsx";
+import Pagination from "./IndexSections/Pagination.jsx";
+import Pills from "./IndexSections/Pills.jsx";
+import Labels from "./IndexSections/Labels.jsx";
+import Alerts from "./IndexSections/Alerts.jsx";
+import Typography from "./IndexSections/Typography.jsx";
+import Modals from "./IndexSections/Modals.jsx";
+import Datepicker from "./IndexSections/Datepicker.jsx";
+import TooltipPopover from "./IndexSections/TooltipPopover.jsx";
+import Carousel from "./IndexSections/Carousel.jsx";
+import Icons from "./IndexSections/Icons.jsx";
+import Login from "./IndexSections/Login.jsx";
+import Download from "./IndexSections/Download.jsx";
+
+class Index extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+
+          <Carousel />
+          <Hero />
+          <section className="section">
+            <Container>
+              <CustomControls />
+              <Menus />
+            </Container>
+          </section>
+          <Buttons />
+          <Inputs />
+
+          <Navbars />
+          <section className="section section-components">
+            <Container>
+              <Tabs />
+              <Row className="row-grid justify-content-between align-items-center mt-lg">
+                <Progress />
+                <Pagination />
+              </Row>
+              <Row className="row-grid justify-content-between">
+                <Pills />
+                <Labels />
+              </Row>
+              <Alerts />
+              <Typography />
+              <Modals />
+              <Datepicker />
+              <TooltipPopover />
+            </Container>
+          </section>
+
+          <Icons />
+          <Login />
+          <Download />
+        </main>
+        <CardsFooter />
+      </>
+    );
+  }
+}
+
+export default Index;
---
+++ src/views/IndexSections/Modals.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classnames from "classnames";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Modal,
+  Row,
+  Col
+} from "reactstrap";
+
+class Modals extends React.Component {
+  state = {};
+  toggleModal = state => {
+    this.setState({
+      [state]: !this.state[state]
+    });
+  };
+  render() {
+    return (
+      <>
+        <h2 className="mt-lg mb-5">
+          <span>Javascript Components</span>
+        </h2>
+        <h3 className="h4 text-success font-weight-bold mb-4">Modals</h3>
+        <Row>
+          <Col md="2">
+            <Button
+              block
+              className="mb-3"
+              color="warning"
+              type="button"
+              onClick={() => this.toggleModal("notificationModal")}
+            >
+              Notification
+            </Button>
+            <Modal
+              className="modal-dialog-centered modal-danger"
+              contentClassName="bg-gradient-danger"
+              isOpen={this.state.notificationModal}
+              toggle={() => this.toggleModal("notificationModal")}
+            >
+              <div className="modal-header">
+                <h6 className="modal-title" id="modal-title-notification">
+                  Your attention is required
+                </h6>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("notificationModal")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="py-3 text-center">
+                  <i className="ni ni-bell-55 ni-3x" />
+                  <h4 className="heading mt-4">You should read this!</h4>
+                  <p>
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary regelialia.
+                  </p>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <Button className="btn-white" color="default" type="button">
+                  Ok, Got it
+                </Button>
+                <Button
+                  className="text-white ml-auto"
+                  color="link"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("notificationModal")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
+          </Col>
+
+        </Row>
+      </>
+    );
+  }
+}
+
+export default Modals;
