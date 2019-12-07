4fc08b4ff0bdc3bf7da82b85c27030fe29eed135
Liubomir Djantov <ldjantov@gmail.com>
2019-12-07 15:24:10 +0200

    updated user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const encryption = require('../utilities/encryption')
 
 let userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    username: { type: String, required: true },
+    username: { type: String, required: true, trim: true, minlength: 3 },
     salt: String,
     password: String,
     roles: [String],
