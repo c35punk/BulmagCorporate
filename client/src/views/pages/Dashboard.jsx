524219193d738ff058f293772fc2fda816a078a9
Liubomir Djantov <ldjantov@gmail.com>
2019-12-12 13:13:58 +0200

    Dashboard.jsx

--- client/src/views/pages/Dashboard.jsx
+++ client/src/views/pages/Dashboard.jsx
@@ -175,71 +175,72 @@ class Dashboard extends React.Component {
               </Row>
             </Container>
           </section>
-          <Row className="row-grid align-items-center">
-            <Col className="order-lg-2 ml-lg-auto" md="6">
-              <div className="position-relative pl-md-5">
-                <img
-                  alt="..."
-                  className="img-center img-fluid"
-                  src={require("assets/img/ill/ill-2.svg")}
-                />
-              </div>
-            </Col>
-            <Col className="order-lg-1" lg="6">
-              <Card className="shadow shadow-lg--hover mt-5">
-                <CardBody>
-                  <div className="d-flex px-3">
-                    <div>
-                      <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
-                        <i className="ni ni-satisfied" />
-                      </div>
-                    </div>
-                    <div className="pl-4">
-                      <h5 className="title text-success">Awesome Support</h5>
-                      <p>
-                        The Arctic Ocean freezes every winter and much of the
-                        sea-ice then thaws every summer, and that process will
-                        continue whatever.
-                      </p>
-                      <a
-                        className="text-success"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        Learn more
-                      </a>
-                    </div>
+          <section>
+            <Container>
+              <Row className="row-grid align-items-center">
+                <Col className="order-lg-2 ml-lg-auto" md="6">
+                  <div className="position-relative pl-md-5">
+                    <img
+                      alt="..."
+                      className="img-center img-fluid"
+                      src={require("assets/img/ill/ill-2.svg")}
+                    />
                   </div>
-                </CardBody>
-              </Card>
-              <Card className="shadow shadow-lg--hover mt-5">
-                <CardBody>
-                  <div className="d-flex px-3">
-                    <div>
-                      <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                        <i className="ni ni-active-40" />
+                </Col>
+                <Col className="order-lg-1" lg="6">
+                  <Card className="shadow shadow-lg--hover mt-5">
+                    <CardBody>
+                      <div className="d-flex px-3">
+                        <div>
+                          <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
+                            <i className="ni ni-satisfied" />
+                          </div>
+                        </div>
+                        <div className="pl-4">
+                          <h5 className="title text-success">
+                            High-end Products
+                          </h5>
+                          <p>
+                            Bulmag AD offer full range of Lenovo®, NetApp®,
+                            INFINIDAT®, VMware®, RedHat®, Microsoft®, Cisco®,
+                            TransactionSystems®, PAX®, and Datecs® products.
+                          </p>
+                          <Button className="text-warning" to="/products" tag={Link}>
+                            Learn more
+                          </Button>
+                        </div>
                       </div>
-                    </div>
-                    <div className="pl-4">
-                      <h5 className="title text-warning">Modular Components</h5>
-                      <p>
-                        The Arctic Ocean freezes every winter and much of the
-                        sea-ice then thaws every summer, and that process will
-                        continue whatever.
-                      </p>
-                      <a
-                        className="text-warning"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        Learn more
-                      </a>
-                    </div>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
+                    </CardBody>
+                  </Card>
+                  <Card className="shadow shadow-lg--hover mt-5">
+                    <CardBody>
+                      <div className="d-flex px-3">
+                        <div>
+                          <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                            <i className="ni ni-active-40" />
+                          </div>
+                        </div>
+                        <div className="pl-4">
+                          <h5 className="title text-warning">
+                            Boost you business with our Services
+                          </h5>
+                          <p>
+                            We are maintaining the tools and resources needed to
+                            deliver our services more effectively yielding great
+                            returns, more efficient work cycles, while achieving
+                            positive impact at the end of the day.
+                          </p>
+                          <Button className="text-warning" to="/services" tag={Link}>
+                            Learn more
+                          </Button>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </section>
         </main>
       </>
     );
