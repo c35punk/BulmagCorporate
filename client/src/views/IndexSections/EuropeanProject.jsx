d1389621eb0d7925bf17e4140b0390a99df109b9
Liubomir Djantov <ldjantov@gmail.com>
2020-03-04 17:21:17 +0200

    added EuropeanProjects.jsx

---
+++ client/src/views/IndexSections/EuropeanProject.jsx
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+
+import { Container, Row, Col, Label, Input } from "reactstrap";
+
+import { isMobile } from "react-device-detect";
+
+const EuropeanProjectInfo = () => {
+  const [checked, setChecked] = useState(false);
+  const handleClick = () => setChecked(!checked);
+
+  let image = isMobile ? (
+    <Col>
+      <img
+        src="https://katina.bg/wp-content/uploads/2018/04/bl-euro.jpg"
+        alt="..."
+        style={{
+          width: "330px",
+          height: "455px",
+          objectFit: "contain"
+        }}
+      />
+    </Col>
+  ) : (
+    <Col>
+      <img
+        src="https://katina.bg/wp-content/uploads/2018/04/bl-euro.jpg"
+        alt="..."
+      />
+    </Col>
+  );
+
+  let languageVersion = checked ? (
+    <Row id="bgVer">
+      <p>
+        На 29.03.2016 г. <strong>Булмаг АД</strong> подписа договор за
+        безвъзмездни средства по{" "}
+        <strong>
+          Оперативна програма „Иновации и конкурентоспособност“ 2014-2020
+          процедура „Подобряване на производствения капацитет на МСП“.
+        </strong>
+        .
+      </p>
+      <p>
+        Общата стойност на проекта е <strong>1 088 520,00 лева</strong>, от
+        които 637 491,73 лева, европейско финансиране, 112 498,55 лева.
+        национално финансиране и 338 529,72 лв., самофинансиране от
+        бенефициента.
+      </p>
+      <p>
+        Основната цел на проектното предложение е да създаде условия за
+        осъществяване на експортна дейност и по този начин да увеличи
+        производствения капацитет на предприятието под формата на увеличаване на
+        обема на предоставяните услуги. Това ще бъде постигнато чрез въвеждането
+        на нова високотехнологична уеб-базирана софтуерна платформа за
+        автоматизирано съхранение на потребителска информация (архивиране и
+        възстановяване) на отдалечен сървър. Уеб базирана версия на платформата
+        ще осигури достъп до услугата от всяка точка на света, което ще даде
+        възможност за износ на тази конкретна услуга. Предоставянето на услугата
+        не е свързано с физическото присъствие на служител на компанията или
+        инсталацията на лаптопа на клиента, което ще оптимизира производствения
+        процес чрез намаляване на ангажираното работно време и също така ще
+        подобри ефективността на разходите за ресурси.
+      </p>
+      <param />
+      По време на изпълнението на проектното предложение, заедно с основната
+      цел, ще бъдат постигнати още няколко съпътстващи конкретни цели:
+      <p>
+        1. Увеличаване на продажбите; 2. Разширяване на пазарния дял; 3.
+        Международният пазар получи приходи. 6-10% от нетния доход на компанията
+        да се извлича от международните пазари като краткосрочна цел; 4.
+        Увеличаване на производителността на труда; 5. Подобряване на
+        ефективността на ресурсите.
+      </p>
+      <p>
+        Успешното изпълнение на проекта и постигането на целите са предпоставка
+        за постигане на силно развитие в Булмаг АД. Всичко това ще увеличи
+        значително конкурентоспособността на компанията и ще позволи
+        заграбването на солидни международни пазарни позиции.
+      </p>
+      <p>
+        Постигането на тези цели пряко съответства на целите на тази процедура
+        „за увеличаване на производствения капацитет и увеличаване на експортния
+        потенциал“. Изпълнение на целите на проекта и оптимизиране на
+        производствените процеси и разширяване на потенциала на компанията до
+        по-широк спектър от услуги. Това съответства на другия край на ОП
+        „Иновации и конкурентоспособност“ 2014-2020 г., а именно „устойчив
+        растеж чрез насърчаване на по-конкурентна икономика с по-ефективно
+        използване на ресурсите“.
+      </p>
+    </Row>
+  ) : (
+    <Row id="enVer">
+      <p>
+        On 29/03/2016, the <strong>Bulmag AD</strong> signed a contract for
+        grants under the{" "}
+        <strong>
+          Operational Programme "Innovation and Competitiveness" 2014-2020
+          procedure "Improving the production capacity of SMEs"
+        </strong>
+        .
+      </p>
+      <p>
+        The total cost of the project is
+        <strong>1 088 520.00 BGN</strong>, of which 637 491.73 BGN, european
+        funding, 112 498.55 BGN. national funding and 338 529.72 BGN,
+        self-financing by the beneficiary.
+      </p>
+      <p>
+        The main objective of the project proposal is to create conditions for
+        implementation of export activity and thus increase the production
+        capacity of the enterprise in the form of increasing the volume of
+        services provided. This will be achieved through the introduction of new
+        high-tech web-based software platform for automated storage of user
+        information (Backup and Restore) to a remote server. Web-based version
+        of the platform will provide access to the service from anywhere in the
+        world, which will enable the export of that particular service.
+        Delivering the service is not related with the physical presence of
+        employee of the company or installation on customer’s laptop, which will
+        optimize the manufacturing process by reducing the committed labor time
+        and also will improve the efficiency of resource costs.
+      </p>
+      <param />
+      During the execution of the project proposal along with the main objective
+      several other accompanying specific objectives will be met:
+      <p>
+        1. Increase sales; 2. Expand market share; 3. International market
+        obtained revenue. 6-10% of the net income of the company to be derived
+        from international markets as a short-term goal; 4. Increase labor
+        productivity; 5. Improve resource efficiency.
+      </p>
+      <p>
+        The successful project implementation and achievement of goals are a
+        prerequisite of achieving a strong development at Bulmag AD. All this
+        will increase significantly the competitiveness of the company and will
+        allow the seizure of solid international market positions.
+      </p>
+      <p>
+        Achieving these objectives directly corresponds with the objectives of
+        this procedure "to increase the production capacity and enhance export
+        potential." Implementation of project goals and optimize production
+        processes and expand the potential of the company to a wider range of
+        services. This corresponds to the other end of the OP "Innovation and
+        Competitiveness" 2014-2020, namely "sustainable growth by fostering a
+        more competitive economy with more efficient use of resources".
+      </p>
+    </Row>
+  );
+
+  console.log(checked);
+
+  return (
+    <section className="section section-lg pt-lg-0 mt--200">
+      <Container className="py-lg-md d-flex">
+        <div className="col px-2">
+          {image}
+          <Row>
+            <Col className="justify-content-center align-items-center">
+              <Label className="custom-toggle">
+                <Input
+                  type="checkbox"
+                  onClick={handleClick}
+                  checked={checked}
+                />
+                <span className="custom-toggle-slider rounded-circle" />
+              </Label>
+            </Col>
+          </Row>
+          {languageVersion}
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default EuropeanProjectInfo;
--- client/src/views/pages/Projects.jsx
+++ client/src/views/pages/Projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 // reactstrap components
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Input, Label } from "reactstrap";
 import { Parallax } from "react-parallax";
+import { isMobile } from "react-device-detect";
+import EuropeanProjectInfo from "views/IndexSections/EuropeanProject";
 
 class Projects extends React.Component {
   state = {};
@@ -12,20 +14,41 @@ class Projects extends React.Component {
     this.refs.main.scrollTop = 0;
   }
   render() {
+    let image = isMobile ? (
+      <Col>
+        <img
+          src="https://katina.bg/wp-content/uploads/2018/04/bl-euro.jpg"
+          alt="..."
+          style={{
+            width: "330px",
+            height: "455px",
+            objectFit: "contain"
+          }}
+        />
+      </Col>
+    ) : (
+      <Col>
+        <img
+          src="https://katina.bg/wp-content/uploads/2018/04/bl-euro.jpg"
+          alt="..."
+        />
+      </Col>
+    );
+
     return (
       <>
         <main ref="main" className="text-darker">
           <div className="position-relative">
             <Parallax
-              blur={{ min: -15, max: 15 }}
+              blur={{ min: -1, max: 1 }}
               bgImage={require("../../assets/img/pics-vte5/IMG_0854.jpg")}
               bgImageAlt="the dog"
-              strength={450}
+              strength={150}
             >
               <div className="shape shape-style-1 shape-default "></div>
               <section className="section section-md section-shaped mb--200">
                 <Container className="py-lg-md d-flex">
-                  <div className="col px-0 mb--200">
+                  <div className="col px-2 mb--200">
                     <Row>
                       <Col lg="6">
                         <h1 className="display-3 text-white">PROJECTS</h1>
@@ -33,107 +56,10 @@ class Projects extends React.Component {
                     </Row>
                   </div>
                 </Container>
-                <div style={{ height: "350px" }} />
+                <div style={{ height: !isMobile ? "350px" : "150px" }} />
               </section>
             </Parallax>
-            {/* shape Hero */}
-
-            <section className="section section-lg pt-lg-0 mt--200">
-              <div className="shape shape-style-1 shape-default"></div>
-              {/* SVG separator */}
-              <div className="separator separator-bottom separator-skew">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                  version="1.1"
-                  viewBox="0 0 2560 100"
-                  x="0"
-                  y="0"
-                >
-                  <polygon
-                    className="fill-white"
-                    points="2560 0 2560 100 0 100"
-                  />
-                </svg>
-              </div>
-              <Container className="py-lg-md d-flex">
-                <div className="col px-0">
-                  <Row>
-                    <p>
-                      <img
-                        src="https://katina.bg/wp-content/uploads/2018/04/bl-euro.jpg"
-                        alt="..."
-                      />
-                    </p>
-                    <hr />
-                    &nbsp;
-                    <p>
-                      On 29/03/2016, the <strong>Bulmag AD</strong> signed a
-                      contract for grants under the{" "}
-                      <strong>
-                        Operational Programme "Innovation and Competitiveness"
-                        2014-2020 procedure "Improving the production capacity
-                        of SMEs"
-                      </strong>
-                      .
-                    </p>
-                    <p>
-                      The total cost of the project is
-                      <strong>1 088 520.00 BGN</strong>, of which
-                      637 491.73 BGN, european funding, 112 498.55 BGN. national
-                      funding and 338 529.72 BGN, self-financing by the
-                      beneficiary.
-                    </p>
-                    <p>
-                      The main objective of the project proposal is to create
-                      conditions for implementation of export activity and thus
-                      increase the production capacity of the enterprise in the
-                      form of increasing the volume of services provided. This
-                      will be achieved through the introduction of new high-tech
-                      web-based software platform for automated storage of user
-                      information (Backup and Restore) to a remote server.
-                      Web-based version of the platform will provide access to
-                      the service from anywhere in the world, which will enable
-                      the export of that particular service. Delivering the
-                      service is not related with the physical presence of
-                      employee of the company or installation on customer’s
-                      laptop, which will optimize the manufacturing process by
-                      reducing the committed labor time and also will improve
-                      the efficiency of resource costs.
-                    </p>
-                    <param />
-                    During the execution of the project proposal along with the
-                    main objective several other accompanying specific
-                    objectives will be met:
-                    <p>
-                      1. Increase sales; 2. Expand market share; 3.
-                      International market obtained revenue. 6-10% of the net
-                      income of the company to be derived from international
-                      markets as a short-term goal; 4. Increase labor
-                      productivity; 5. Improve resource efficiency.
-                    </p>
-                    <p>
-                      The successful project implementation and achievement of
-                      goals are a prerequisite of achieving a strong development
-                      at Bulmag AD. All this will increase significantly the
-                      competitiveness of the company and will allow the seizure
-                      of solid international market positions.
-                    </p>
-                    <p>
-                      Achieving these objectives directly corresponds with the
-                      objectives of this procedure "to increase the production
-                      capacity and enhance export potential." Implementation of
-                      project goals and optimize production processes and expand
-                      the potential of the company to a wider range of services.
-                      This corresponds to the other end of the OP "Innovation
-                      and Competitiveness" 2014-2020, namely "sustainable growth
-                      by fostering a more competitive economy with more
-                      efficient use of resources".
-                    </p>
-                  </Row>
-                </div>
-              </Container>
-            </section>
+            <EuropeanProjectInfo />
           </div>
         </main>
       </>
